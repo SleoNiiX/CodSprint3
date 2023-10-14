54d4fac5787b04f012047c5f996b28b6fc9c2f7e
SleoNiiX <cardosomoreira.anthony@gmail.com>
2023-10-14 21:17:32 +0200

    choix 3

--- interface_graphique.py
+++ interface_graphique.py
@@ -1,5 +1,6 @@
 from main import *
 from tkinter import *
+import datetime
 
 
 
@@ -18,16 +19,18 @@ def valider():
 
     
     if date_valide(int(jour.get()), int(mois.get()), int(annee.get())):
-        annee = int(annee.get())
-        mois = int(mois.get())
-        jour = int(jour.get())
 
         if choix == 1:
             liste_jour = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
 
-            text = 'Le ' + chaine_date(jour, mois, annee) + ' est un ' + liste_jour[nb_jour_entre_dates(21, 7, 1969, jour, mois ,annee)%7] + '.'
+            text = 'Le ' + chaine_date(int(jour.get()), int(mois.get()), int(annee.get())) + ' est un ' + liste_jour[nb_jour_entre_dates(21, 7, 1969, int(jour.get()), int(mois.get()), int(annee.get()))%7] + '.'
             reponse = Label(boite_affichage, text=text, font=('Courrier', 10, 'bold'))   
-            reponse.pack()        
+            reponse.pack()    
+        elif choix == 3:
+            aujourd_hui = datetime.date.today()
+            text = 'Entre le ' + chaine_date(int(jour.get()), int(mois.get()), int(annee.get())) + ' et le ' + chaine_date(aujourd_hui.day, aujourd_hui.month, aujourd_hui.year) + ' il y a ' + str(nb_jour_entre_dates(int(jour.get()), int(mois.get()), int(annee.get()), aujourd_hui.day, aujourd_hui.month, aujourd_hui.year)) + ' jours.'
+            reponse = Label(boite_affichage, text=text, font=('Courrier', 10, 'bold'))    
+            reponse.pack()
     else:
         label_invalide.pack()
     
@@ -76,16 +79,47 @@ def choix2():
     label.pack()
 
 def choix3():
-    pass
+
+    global boite_affichage
+    global choix
+
+    choix = 3
+
+    global annee
+    global mois
+    global jour
+
+    boite_affichage.destroy()
+
+    boite_affichage = Frame(fenetre)
+    boite_affichage.pack(side=RIGHT)
+    
+    label_naissance = Label(boite_affichage, text='Entrez votre date de naissance :', font=('Courrier',12))
+    label_naissance.pack()
+
+    annee = Entry(boite_affichage)
+    annee.insert(0, "Entrez l'annee...")
+    annee.pack()  
+
+    mois = Entry(boite_affichage)
+    mois.insert(0, "Entrez le mois...")
+    mois.pack() 
+
+    jour = Entry(boite_affichage)
+    jour.insert(0, "Entrez le jour...")
+    jour.pack() 
+
+    bouton_valider = Button(boite_affichage, text='Valider', command=valider)
+    bouton_valider.pack()
 
 def choix4():
-    pass
+    fenetre.destroy()
 
 
 fenetre = Tk()
 fenetre.title("Calendrier Python")
-fenetre.geometry("480x360")
-fenetre.minsize(480, 360)
+fenetre.geometry("560x360")
+fenetre.minsize(560, 360)
 
 boite_choix = Frame(fenetre)
 boite_affichage = Frame(fenetre)
@@ -112,4 +146,3 @@ bouton4.pack(fill=X)
 boite_choix.pack(side=LEFT)
 
 fenetre.mainloop()
-
