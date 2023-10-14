073d14582e48ee1faa3429d6446d70b645403414
SleoNiiX <cardosomoreira.anthony@gmail.com>
2023-10-14 15:19:27 +0200

    Programme principal fini

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import datetime
+
+
+
 def annee_bissextile(annee):
     return annee%400 == 0 or (annee%4 == 0 and annee%100 != 0)
 
@@ -60,6 +64,14 @@ def nb_jour_annee_entiere(annee1,annee2):
 
     return nb_jour_annee_entiere
 
+def nb_jour_mois_entier(mois1, mois2,annee):
+    nb_jour_mois_entier = 0
+
+    for mois in range(mois1+1,mois2):
+        nb_jour_mois_entier += nb_jour_mois(mois,annee)
+
+    return nb_jour_mois_entier
+
 def nb_jour_entre_dates(jour1, mois1, annee1, jour2, mois2 ,annee2):
     date_anterieure = False
 
@@ -71,9 +83,83 @@ def nb_jour_entre_dates(jour1, mois1, annee1, jour2, mois2 ,annee2):
         date_anterieure = True
 
     if date_anterieure:
+        if annee1 == annee2:
+            if annee1 == annee2 and mois1 == mois2:
+                return jour2 - jour1
+            return nb_jour_mois(mois1, annee1) - jour1 + nb_jour_mois_entier(mois1, mois2, annee1) + jour2
+        
         return nb_jour_fin_annee(jour1, mois1, annee1) + nb_jour_annee_entiere(annee1,annee2) + nb_jour_debut_annee(jour2, mois2, annee2)
+    
     elif date_anterieure is False:
+        if annee1 == annee2:
+            if annee1 == annee2 and mois1 == mois2:
+                return jour2 - jour1
+            return -(nb_jour_mois(mois2, annee2) - jour2 + nb_jour_mois_entier(mois2, mois1, annee2) + jour1)
+    
         return -(nb_jour_fin_annee(jour2, mois2 ,annee2) + nb_jour_annee_entiere(annee2,annee1) + nb_jour_debut_annee(jour1, mois1, annee1))
     
-print(nb_jour_entre_dates(12, 1, 2009, 25, 11, 2006))
-print(nb_jour_entre_dates(25, 11, 2006, 12, 1, 2009))
+#print(nb_jour_entre_dates(18, 7, 2022, 14, 10 ,2023))
+Ouverture_menu = True
+
+while Ouverture_menu:
+    choix = int(input("\necrire le chiffre correspondant a choix : \n\n1. Calcul du jour de la semaine correspondant a une date\n2. Calcul du nombre de jours separant deux dates\n3. Calcul du nombre de jours ecoules depuis votre naissance\n4. Sortie du programme\n... "))
+
+    if choix == 4:
+        Ouverture_menu = False
+
+    elif choix == 1:
+        date_invalide = True
+        liste_jour = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+
+        while date_invalide:
+            annee = int(input("Entre l'annee : "))
+            mois = int(input("Entre le mois : "))
+            jour = int(input("Entre le jour : "))
+
+            if date_valide(jour, mois, annee):
+                date_invalide = False
+            else:
+                print("date invalide...")
+
+        print('\nLe', chaine_date(jour, mois, annee), 'est un',
+               liste_jour[nb_jour_entre_dates(21, 7, 1969, jour, mois ,annee)%7], end='.')
+
+    elif choix == 2:    
+        date_invalide = True
+
+        while date_invalide:
+            annee1 = int(input("Entre l'annee de ta premiere date : "))
+            mois1 = int(input("Entre le mois de ta premiere date : "))
+            jour1 = int(input("Entre le jour de ta premiere date : "))
+            annee2 = int(input("Entre l'annee de ta deuxieme date : "))
+            mois2 = int(input("Entre le mois de ta deuxieme date : "))
+            jour2 = int(input("Entre le jour de ta deuxieme date : "))
+
+            if date_valide(jour1, mois1, annee1) is False:
+                print("premiere date invalide...")
+            if date_valide(jour2, mois2, annee2) is False:
+                print("deuxieme date invalide...")
+            if date_valide(jour1, mois1, annee1) and date_valide(jour2, mois2, annee2):
+                date_invalide = False
+
+        print('\nEntre le', chaine_date(jour1, mois1, annee1), 'et le', chaine_date(jour2, mois2, annee2),
+               'il y a', nb_jour_entre_dates(jour1, mois1, annee1, jour2, mois2 ,annee2), 'jours.')
+        
+    elif choix == 3:
+        aujourd_hui = datetime.date.today()
+        date_invalide = True
+
+        while date_invalide:
+            annee = int(input("Entre ton annee de naissance : "))
+            mois = int(input("Entre ton mois de naissance : "))
+            jour = int(input("Entre ton jour de naissance : "))
+
+            if date_valide(jour, mois, annee):
+                date_invalide = False
+            else:
+                print("date invalide...")
+
+        print('\nEntre le', chaine_date(jour, mois, annee), 'et le',
+               chaine_date(aujourd_hui.day, aujourd_hui.month, aujourd_hui.year),'il y a',
+                nb_jour_entre_dates(jour, mois, annee, aujourd_hui.day, aujourd_hui.month, aujourd_hui.year), 'jours.')
+            
